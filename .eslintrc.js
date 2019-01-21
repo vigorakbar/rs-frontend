897625ce209120c8c8a86b06689755c86c5a765b
vigor <vigorakbar@gmail.com>
2019-01-22 00:18:27 +0700

    setting eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,14 @@ module.exports = {
         "node": true,
         "jasmine": true
       },
+      "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      },
+      "settings": {
+        "import/resolver": {
+          "node": {
+            "paths": ["src"]
+          }
+        },
+      },
 };
