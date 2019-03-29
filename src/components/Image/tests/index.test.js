3c81ce96ed590e2fe80ee8e730b4b4a9aabefc5f
Dzar Bela Hanifa <dzarbelahanifa@gmail.com>
2019-03-29 21:12:51 +0700

    fix string quote

--- src/components/Image/tests/index.test.js
+++ src/components/Image/tests/index.test.js
@@ -5,27 +5,27 @@ import Image from '../index';
 
 describe('<Image />', () => {
   it('should render a <img> tag', () => {
-    const renderedComponent = shallow(<Image src='google.com' alt="alt" />);
+    const renderedComponent = shallow(<Image src="google.com" alt="alt" />);
     expect(renderedComponent.type()).toEqual('img');
   });
 
   it('should adopt src attribute', () => {
-    const renderedComponent = shallow(<Image src='mamamoo.com' alt="alt" />);
-    expect((renderedComponent.find('img')).prop('src')).toEqual("mamamoo.com");
+    const renderedComponent = shallow(<Image src="mamamoo.com" alt="alt" />);
+    expect((renderedComponent.find('img')).prop('src')).toEqual('mamamoo.com');
   });
 
   it('should have className attribute', () => {
-    const renderedComponent = shallow(<Image src='mamamoo.com' alt="alt" className="email" />);
+    const renderedComponent = shallow(<Image src="mamamoo.com" alt="alt" className="email" />);
     expect((renderedComponent.find('img')).prop('className')).toEqual('email');
   });
 
   it('should have srcSet attribute', () => {
-    const renderedComponent = shallow(<Image src='mamamoo.com' srcSet="bambang" alt="alt" />);
+    const renderedComponent = shallow(<Image src="mamamoo.com" srcSet="bambang" alt="alt" />);
     expect((renderedComponent.find('img')).prop('srcSet')).toEqual('bambang');
   });
 
   it('should have alt attribute', () => {
-    const renderedComponent = shallow(<Image src='mamamoo.com' alt="alit" />);
+    const renderedComponent = shallow(<Image src="mamamoo.com" alt="alit" />);
     expect((renderedComponent.find('img')).prop('alt')).toEqual('alit');
   });
 });
