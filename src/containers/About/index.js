8fb866f7c23fd02cf00963e7f1f7a5bac6084de5
vigor <vigorakbar@gmail.com>
2019-04-09 22:25:09 +0700

    add Footer component

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Header from 'components/Header';
+import Footer from 'components/Footer';
 
 const About = () => (
   <div>
     <Header active="about-us" />
+    this is about page<br /> <br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
+    this is about page<br /><br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
+    this is about page<br /><br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
+    this is about page<br /><br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
     this is about page
+    <Footer profile={{
+      name: 'Rumah Belajar Sahaja',
+      address: 'Jalan Ciroyom, Bandung',
+      phone: '081081081081',
+      email: 'sahaja@rumbel.com',
+    }}
+    />
   </div>
 );
 
