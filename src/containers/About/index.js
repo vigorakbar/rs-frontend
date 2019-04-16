76645e2874cacb27f924f1086f42c918c4c0fc1f
vigor <vigorakbar@gmail.com>
2019-04-16 23:59:22 +0700

    update about page

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -2,22 +2,43 @@ import React from 'react';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
-const About = () => (
-  <div>
-    <Header active="about-us" />
-    this is about page<br /> <br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
-    this is about page<br /><br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
-    this is about page<br /><br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
-    this is about page<br /><br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
-    this is about page
-    <Footer profile={{
-      name: 'Rumah Belajar Sahaja',
-      address: 'Jalan Ciroyom, Bandung',
-      phone: '081081081081',
-      email: 'sahaja@rumbel.com',
-    }}
-    />
-  </div>
-);
+import { getProfile } from 'models/Profile';
+
+class About extends React.PureComponent {
+    state = {
+      profile: {},
+    }
+
+    componentDidMount() {
+      this.getRSProfile();
+    }
+
+    getRSProfile = () => {
+      getProfile()
+        .then((response) => {
+          this.setState({
+            profile: response.data,
+          });
+        })
+        .catch((error) => {
+          console.log(error) //eslint-disable-line
+        });
+    }
+
+    render() {
+      const { profile } = this.state;
+      return (
+        <div>
+          <Header active="about-us" />
+          this is about page<br /> <br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
+          this is about page<br /><br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
+          this is about page<br /><br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
+          this is about page<br /><br /><br /><br /><br /><br /><br /><br />{/* eslint-disable-line */}
+          this is about page
+          <Footer profile={profile} />
+        </div>
+      );
+    }
+}
 
 export default About;
