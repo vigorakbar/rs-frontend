69d1ac5fafe6494eba0fc6765418a313dfc0c262
vigor <vigorakbar@gmail.com>
2019-04-16 23:59:34 +0700

    add donation page

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from 'containers/Home';
 import About from 'containers/About';
+import Donation from 'containers/Donation';
 import Login from 'containers/admins/Login';
 import AdminTestPage from 'containers/admins/Test';
 import ProtectedRoute from './ProtectedRoute';
@@ -17,6 +18,7 @@ const App = () => (
     <Route exact path="/" component={Home} />
     <Route exact path="/about-us" component={About} />
     <Route exact path="/login" component={Login} />
+    <Route exact path="/donation" component={Donation} />
     <ProtectedRoute exact path="/admin/test" component={AdminTestPage} />
     <Route component={() => renderNotFound()} />
   </Switch>
