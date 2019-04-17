266e58d93fb86a77eaa8f1d39a1c2fcffae7e09f
Dzar Bela Hanifa <dzarbelahanifa@gmail.com>
2019-04-17 22:55:11 +0700

    fix css

--- src/containers/admins/PostNews/index.js
+++ src/containers/admins/PostNews/index.js
@@ -4,17 +4,31 @@ import { EditorState, convertToRaw } from 'draft-js';
 
 import WYSIWYG from 'components/WYSIWYG';
 import BaseButton from 'components/Button';
+import BaseTextField from 'components/forms/TextField';
 
 import { postNews } from 'models/PostNews';
 
 
 const ContainerWYSIWYG = styled.div`
-  border: 1px solid grey;
   padding: 6px;
-  height: 400px !important;
+  height: 400px;
   overflow: auto;
-  width: 1000px;
   margin: auto;
+  border: 1px solid #ced4da;
+  border-radius: .25rem;
+}
+`;
+
+const LabelTextField = styled.div`
+`;
+
+const StyledForm = styled.form`
+  width: 50em;
+  margin: auto;
+`;
+
+const TextField = styled(BaseTextField)`  
+  margin-bottom: 20px;
 `;
 
 const Button = styled(BaseButton)`
@@ -25,13 +39,29 @@ const Button = styled(BaseButton)`
 class PostNews extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty() };
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      title: '',
+      thumbnail: '',
+    };
   }
 
-  handleChange = (editorState) => {
+  handleChangeWYSIWYG = (editorState) => {
     const contentState = editorState.getCurrentContent();
     console.log('content state', convertToRaw(contentState));
     this.setState({ editorState });
+  }
+
+  handleChangeTitle = (title) => {
+    this.setState({
+      title,
+    });
+  }
+
+  handleChangeThumbnail = (thumbnail) => {
+    this.setState({
+      thumbnail,
+    });
   }
 
   handleSubmit = (event) => {
@@ -41,17 +71,28 @@ class PostNews extends React.Component {
   }
 
   render() {
-    const { editorState } = this.state;
+    const { editorState, title, thumbnail } = this.state;
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <ContainerWYSIWYG>
-            <WYSIWYG editorState={editorState} onEditorStateChange={this.handleChange} />
-          </ContainerWYSIWYG>
+        <StyledForm onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <LabelTextField>Judul Artikel</LabelTextField>
+            <TextField type="text" value={title} placeholder="Judul artikel" onValueChange={this.handleChangeTitle} />
+          </div>
+          <div className="form-group">
+            <LabelTextField>Thumbnail Gambar</LabelTextField>
+            <TextField type="text" value={thumbnail} placeholder="Link menuju gambar" onValueChange={this.handleChangeThumbnail} />
+          </div>
+          <div className="form-group">
+            <LabelTextField>Konten Artikel</LabelTextField>
+            <ContainerWYSIWYG>
+              <WYSIWYG editorState={editorState} onEditorStateChange={this.handleChangeWYSIWYG} />
+            </ContainerWYSIWYG>
+          </div>
           <Button buttonStyle="success" buttonSize="lg">
             POST !
           </Button>
-        </form>
+        </StyledForm>
       </div>
     );
   }
