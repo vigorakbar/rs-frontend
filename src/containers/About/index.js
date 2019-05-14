2ae460a4ea3c62c5ea4432705df6146753f05395
Dzar Bela Hanifa <dzarbelahanifa@gmail.com>
2019-05-14 15:53:49 +0700

    fix responsive display

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -10,16 +10,15 @@ import BackgroundImgTitle from 'assets/bg-about-title.jpg';
 import LogoRS from 'assets/logo.png';
 import BackgroundImgVisi from 'assets/bg-about-visi.jpg';
 import BackgroundImgMisi from 'assets/bg-about-misi.jpg';
-
-const LogoContainer = Styled.div`
-  margin: 50px auto 50px auto;
-  width: 35em;
-  height: 10em;
-`;
+import BackgroundImgVisiSm from 'assets/bg-about-visi-small.jpg';
+import BackgroundImgMisiSm from 'assets/bg-about-misi-small.jpg';
 
 const Logo = Styled(Image)`
-  height: 100%;
-  width: 100%;
+  height: 75%;
+  width: 75%;
+  @media only screen and (max-width: 500px) {
+    margin-left: 2em;
+  }
 `;
 
 const Container = Styled.div`
@@ -32,102 +31,141 @@ const Container = Styled.div`
 `;
 
 const InnerContainer = Styled.div`
-  width: 100em;
   margin: 0 auto;
+  padding: 0 10vw;
+
+  @media only screen and (max-width: 500px) {
+    padding: 0;
+  }
 
   h2 {
-    margin: 100px 0;
+    margin: 50px 0;
     text-align: center;
-    font-family: 'Lucida Sans Unicode', sans-serif;
+    font-family: 'PT Sans',sans-serif;
     color: #90c9c8;
-    font-size: 32px;
+    font-size: 22px;
     font-weight: bold;
+    @media only screen and (max-width: 500px) {
+      padding: 0 0.5em;
+    }
   }
-`;
 
-const ImageTitleWrapper = Styled.div`
-  height: 50em;
-  width: 100%;
-  background-image: url(${BackgroundImgTitle});
-  background-size: cover;
-  margin: 0 auto;
-`;
+  .logo-container {
+    margin: 50px auto 50px auto;
+    width: 35em;
+    height: 10em;
 
-const ImageContainer = Styled.div`
-  height: 50em;
-  width: 100%;
-  margin: 200px auto;
-  display: flex;
-  position: relative;
-`;
+    @media only screen and (max-width: 500px) {
+      margin: 50px auto 0 auto;
+      width: 20em;
+      height: 5em;
+    }
+  }
 
-const ImageVisiContent = Styled.div`
-background-color: #90c9c8;
-height: 20em;
-width: 40em;
-bottom: 10em;
-right: 0em;
-z-index: 1;
-position: absolute;
-padding: 5em;
+  .image-title-wrapper {
+    height: 50em;
+    width: 100%;
+    background-image: url(${BackgroundImgTitle});
+    background-size: cover;
+    margin: 0 auto;
+    @media only screen and (max-width: 500px) {
+      display: none;
+    }
+  }
 
-h1{    
-  color: #e1eded;
-  font-family: 'Lucida Sans Unicode',sans-serif;
-  font-weight: bold;
-}
+  .image-container {
+    height: 50em;
+    width: 100%;
+    margin: 200px auto;
+    display: flex;
+    position: relative;
+  }
 
-div{
-  color: #e1eded;
-  font-size: 28px;
-  font-family: 'Lucida Sans Unicode',sans-serif;
-  font-weight: bold;
-}
+  .image-visi-wrapper {
+    height: 35em;
+    width: 50em;
+    left: 0em;    
+    background-image: url(${BackgroundImgVisi});
+    background-size: cover;
+    position: absolute;
+    z-index: 0;
 
-`;
+    @media only screen and (max-width: 500px) {
+      background-image: url(${BackgroundImgVisiSm});
+      width: 100%;
+      height: 100%;
+    }
 
-const ImageVisiWrapper = Styled.div`
-  height: 45em;
-  width: 70em;
-  left: 0em
-  background-image: url(${BackgroundImgVisi});
-  background-size: cover;
-  position: absolute;
-  z-index: 0;
-`;
+  }
 
-const ImageMisiContent = Styled.div`
-background-color: #90c9c8;
-height: 40em;
-width: 50em;
-top: 10em;
-left: 0em;
-z-index: 1;
-position: absolute;
-padding: 5em;
-h1{    
-  color: #e1eded;
-  font-family: 'Lucida Sans Unicode',sans-serif;
-  font-weight: bold;
-}
+  .image-visi-content{
+    background-color: #90c9c8;
+    height: 20em;
+    width: 40em;
+    bottom: 10em;
+    right: 0em;
+    z-index: 1;
+    position: absolute;
+    padding: 5em;
+    color: #e1eded;
+    font-family: 'PT Sans',sans-serif;
+    font-weight: bold;
 
-div{
-  color: #e1eded;
-  font-size: 28px;
-  font-family: 'Lucida Sans Unicode',sans-serif;
-  font-weight: bold;
-}
+    @media only screen and (max-width: 500px) {
+      width: 100%;
+    }
 
-`;
+    h1 {
+      font-weight: bold;
+    }
 
-const ImageMisiWrapper = Styled.div`
-  height: 45em;
-  width: 70em;
-  right: 0em;
-  background-image: url(${BackgroundImgMisi});
-  background-size: cover;
-  position: absolute;
-  z-index: 0;
+    div{
+      font-size: 22px;
+    }
+  }
+
+  .image-misi-wrapper{
+    height: 30em;
+    width: 50em;
+    right: 0em;
+    background-image: url(${BackgroundImgMisi});
+    background-size: cover;
+    position: absolute;
+    z-index: 0;
+
+    @media only screen and (max-width: 500px) {
+      background-image: url(${BackgroundImgMisiSm});
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .image-misi-content{
+    background-color: #90c9c8;
+    height: 25em;
+    width: 50em;
+    top: 10em;
+    left: 0em;
+    z-index: 1;
+    position: absolute;
+    padding: 2.5em;
+    color: #e1eded;
+    font-family: 'PT Sans',sans-serif;
+    font-weight: bold;
+  
+    @media only screen and (max-width: 500px) {
+      width: 100%;
+      height: 35em;
+    }
+
+    h1{
+      font-weight: bold;
+    }
+
+    div{
+      font-size: 22px;
+    }
+  }
 `;
 
 class About extends React.PureComponent {
@@ -157,28 +195,28 @@ class About extends React.PureComponent {
       <Container>
         <Header active="about-us" />
         <InnerContainer>
-          <LogoContainer>
+          <div className="logo-container">
             <Logo src={LogoRS} />
-          </LogoContainer>
+          </div>
           <h2>
             Rumah Belajar SAHAJA Ciroyom merupakan sebuah rumah belajar yang didirikan untuk
             mendidik perilaku ‘anak marginal’ agar menjadi lebih baik, berakhlak mulia, berbudi
             pekerti luhur, mandiri, memperoleh pendidikan yang layak dan dapat diterima oleh
             masyarakat.
           </h2>
-          <ImageTitleWrapper />
-          <ImageContainer>
-            <ImageVisiWrapper />
-            <ImageVisiContent>
+          <div className="image-title-wrapper" />
+          <div className="image-container">
+            <div className="image-visi-wrapper" />
+            <div className="image-visi-content">
               <h1>VISI</h1>
               <div>
                 Menjadi sahabat anak-anak marginal untuk membangun karakter yang lebih baik.
               </div>
-            </ImageVisiContent>
-          </ImageContainer>
-          <ImageContainer>
-            <ImageMisiWrapper />
-            <ImageMisiContent>
+            </div>
+          </div>
+          <div className="image-container">
+            <div className="image-misi-wrapper" />
+            <div className="image-misi-content">
               <h1>MISI</h1>
               <div>
                 <br />
@@ -190,8 +228,8 @@ class About extends React.PureComponent {
                 <br />
                 Memfasilitasi anak-anak marginal untuk mendapatkan lingkungan yang lebih baik dalam bentuk rumah singgah.
               </div>
-            </ImageMisiContent>
-          </ImageContainer>
+            </div>
+          </div>
         </InnerContainer>
         <Footer profile={profile} />
       </Container>
